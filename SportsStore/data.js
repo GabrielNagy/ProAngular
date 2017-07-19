3ab8dee3655cb80b736c52d31844bcde502eea10
Gabriel Nagy <gabrielnagy@me.com>
2017-07-19 15:49:47 +0300

    End of Chapter 7

---
+++ SportsStore/data.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+  return {
+    products: [
+      { id: 1, name: "Kayak", category: "Watersports", description: "A boat for one person", price: 275 },
+      { id: 2, name: "Lifejacket", category: "Watersports", description: "Protective and fashionable", price: 48.95 },
+      { id: 3, name: "Soccer Ball", category: "Soccer", description: "FIFA-approved size and weight", price: 19.50 },
+      { id: 4, name: "Stadium", category: "Soccer", description: "Flat-packed 35,000-seat stadium", price: 79000 },
+      { id: 5, name: "Thinking Cap", category: "Chess", description: "Improve brain efficiency by 80%", price: 16 },
+      { id: 6, name: "Unsteady Chair", category: "Chess", description: "Give your opponent a disadvantage", price: 29.95 },
+      { id: 7, name: "Human Chess Board", category: "Chess", description: "A fun game for the family", price: 75 },
+      { id: 8, name: "Yeezy 350", category: "Lifestyle", description: "Best shoes in the game", price: 300 }
+    ],
+    orders: []
+  }
+}
